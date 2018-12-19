27b62d884be2d68154b73f8b95d7a7d1b721d7e3
giinsen <gabriel.lapegue@gmail.com>
2018-12-19 16:20:34 +0100

    staser

---
+++ Assets/Scripts/Staser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Staser : Tool
+{
+	protected override void ActiveAction()
+	{
+		foreach(Ingredient ing in ingredients)
+        {
+            ing.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+	}
+}
